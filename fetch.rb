90a7df6dc6ca539f9824940ed35aa955c92c1fc9
Andreas Krey <a.krey@gmx.de>
2014-12-02 20:12:50 +0100

    fetch.rb: reorg storage

--- fetch.rb
+++ fetch.rb
@@ -7,17 +7,48 @@ res=nil
 
 IO.popen("/usr/bin/wget --tries=1 -O - 'http://www.spritpreismonitor.de/suche/?tx_spritpreismonitor_pi1[searchRequest][plzOrtGeo]=89073&tx_spritpreismonitor_pi1[searchRequest][umkreis]=5&tx_spritpreismonitor_pi1[searchRequest][kraftstoffart]=e5&tx_spritpreismonitor_pi1[searchRequest][tankstellenbetreiber]='") do |f|
   f.each_line do |l|
-	if l =~ /var spmResult = (\[.*\]);/
-		res=$1
-	end
+    if l =~ /var spmResult = (\[.*\]);/
+      res=$1
+    end
   end
 end
 
 js=JSON.parse(res)
 t=Time.now().to_i
+out={ 't' => t}
+stm={}
+
+js.each do |j|
+  begin
+    out[j['mtsk_id']]=j['e5'].to_f
+  rescue e
+    # nix
+  end
+  stm[j['mtsk_id']]=j
+end
+
 s=(t%86400).to_s
 fnm="data/#{t/86400}"
 File.open("#{fnm}.jsa","a") do |f|
-  f.puts JSON.generate(js)+"\n"
+  f.puts JSON.generate(out)+"\n"
+end
+File.open("stm.json","w") do |f|
+  f.puts JSON.generate(stm)+"\n"
+end
+
+fn=[fnm]
+begin
+  File.open("data/flist") do |f|
+    f.each_line do |l|
+      fn<<=l.strip
+    end
+  end
+rescue Errno::ENOENT
+puts fn.inspect
+end
+File.open("data/flist","w") do |f|
+  fn.sort.uniq.reverse[0..7].each do |n|
+    f.puts n
+  end
 end
 

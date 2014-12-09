4f3c28d115d8e6f997dc4741f248da63b29fab7e
Andreas Krey <a.krey@gmx.de>
2014-12-09 16:44:02 +0100

    html header dance

--- do.sh
+++ do.sh
@@ -13,7 +13,50 @@ touch data/flist
 dir=../../html/tanken
 
 cat >$dir/index.html <<\EOF
-<p>Tanken:
+<html>
+<head>
+<title>Tanken in Ulm</title>
+<style>
+.blkhd {
+  margin: 0px;
+  margin-bottom: 10px;
+  border-bottom: 1px solid black;
+  list-style-type: none;
+  background-color: #6666ff;
+  color: black;
+}
+.act {
+  color: black;
+  background-color: lightgray;
+}
+.blk {
+  font-family: sans-serif;
+  display: inline-block;
+  padding: 5px;
+}
+.blk:hover {
+  background-color: lightgray;
+  color: blue;
+}
+.green {
+  background-image: url(http://apk.li/branch.jpg);
+  color: white;
+}
+.undec {
+  text-decoration: none;
+}
+body {
+  margin: 0pt;
+}
+</style>
+</head>
+<body bgcolor="white">
+<ul class="blkhd"><li class="blk"><a class="undec" href="http://parken.apk.li/">Parken</a></li><li class="blk act">Tanken</li>&nbsp;&bullet;&nbsp;<li class="blk green"><a class="undec" href="http://apk.li/"><tt>apk.li</tt></a></li></ul>
+<div style="margin: 8px;">
+<h3>Tanken</h3>
 <p><img src="cost.png">
+</div>
+</body>
+</html>
 EOF
 cp cost.png $dir/cost.png

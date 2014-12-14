09bb96d86183ab92b8b95c0d2f841f2693dab138
Andreas Krey <a.krey@gmx.de>
2014-12-14 10:24:32 +0100

    use external style sheet

--- do.sh
+++ do.sh
@@ -16,43 +16,11 @@ cat >$dir/index.html <<\EOF
 <html>
 <head>
 <title>Tanken in Ulm</title>
-<style>
-.blkhd {
-  margin: 0px;
-  margin-bottom: 10px;
-  border-bottom: 1px solid black;
-  list-style-type: none;
-  background-color: #6666ff;
-  color: black;
-}
-.act {
-  color: black;
-  background-color: lightgray;
-}
-.blk {
-  font-family: sans-serif;
-  display: inline-block;
-  padding: 5px;
-}
-.blk:hover {
-  background-color: lightgray;
-  color: blue;
-}
-.green {
-  background-image: url(http://apk.li/branch.jpg);
-  color: white;
-}
-.undec {
-  text-decoration: none;
-}
-body {
-  margin: 0pt;
-}
-</style>
+<link rel="stylesheet" type="text/css" href="http://ken.apk.li/ken.css">
 </head>
 <body bgcolor="white">
 <ul class="blkhd"><li class="blk"><a class="undec" href="http://parken.apk.li/">Parken</a></li><li class="blk act">Tanken</li>&nbsp;&bullet;&nbsp;<li class="blk green"><a class="undec" href="http://apk.li/"><tt>apk.li</tt></a></li></ul>
-<div style="margin: 8px;">
+<div class="bodybox">
 <h3>Tanken</h3>
 <p><img src="cost.png">
 </div>

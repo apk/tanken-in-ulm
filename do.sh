660eae070be88e7bb906e7fbe54f564b7f799fdb
Andreas Krey <a.krey@gmx.de>
2014-12-18 15:36:34 +0100

    remote tt in apk.li link

--- do.sh
+++ do.sh
@@ -19,7 +19,7 @@ cat >$dir/index.html <<\EOF
 <link rel="stylesheet" type="text/css" href="http://ken.apk.li/ken.css">
 </head>
 <body bgcolor="white">
-<ul class="blkhd"><li class="blk"><a class="undec" href="http://parken.apk.li/">Parken</a></li><li class="blk act">Tanken</li>&nbsp;&bullet;&nbsp;<li class="blk green"><a class="undec" href="http://apk.li/"><tt>apk.li</tt></a></li></ul>
+<ul class="blkhd"><li class="blk"><a class="undec" href="http://parken.apk.li/">Parken</a></li><li class="blk act">Tanken</li>&nbsp;&bullet;&nbsp;<li class="blk green"><a class="undec" href="http://apk.li/">apk.li</a></li></ul>
 <div class="bodybox">
 <h3>Tanken</h3>
 <p><img src="cost.png">
